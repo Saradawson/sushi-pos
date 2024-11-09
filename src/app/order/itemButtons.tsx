9df7aadfac990f482bb7ace8771b9f77b72c0882
unknown <saradawsone@gmail.com>
2024-11-09 14:07:05 -0700

    itemButtons is now a reuseable function to create buttons

--- src/app/order/itemButtons.tsx
+++ src/app/order/itemButtons.tsx
@@ -1,33 +1,52 @@
 'useclient'
+// dummy data
+const items = [
+    {
+        category: "Makis",
+        items: [
+            {name: "Sake Maki", price: 10.00},
+            {name: "Hamachi Maki", price: 10.00},
+            {name: "Maguro Maki", price: 10.00},
+        ],
+    }
+]
 
 interface OrderProps {
-    addItem: (name: string, value: number) => void;
+    addItem: (name: string, price: number) => void
 }
 
 const ItemButtons: React.FC<OrderProps> = ({ addItem }) => {
     
     // Helper function to determine the value of an item button
     const itemClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-        event.preventDefault();
-        const target = event.target as HTMLInputElement;
-        const name = target.name;
-        const value = target.value;
-        addItem(name, Number(value))
+        event.preventDefault()
+        const target = event.target as HTMLInputElement
+        const name = target.name
+        const price = target.getAttribute('data-price')
+        addItem(name, Number(price))
     }
 
     return(
-    <form className="w-1/2 flex flex-col items-center border font-medium">
+        <div className="w-1/2 flex flex-col items-center border font-medium">
             <h2 className="border w-full text-center text-lg">ITEMS</h2>
-            <div className="border w-full pt-5 p-1 flex flex-col items-start gap-2">
-                <h3 className="text-gray-500">Makis</h3>
-                <div>
-                    <button type="button" value={10} name="Maguro Maki" onClick={itemClick} className="border bg-white p-4 rounded-md">Maguro Maki</button>
-                    <button type="button" value={10} name="Sake Maki" onClick={itemClick} className="border bg-white p-4 rounded-md">Sake Maki</button>
-                    <button type="button" value={10} name="Hamachi Maki" onClick={itemClick} className="border bg-white p-4 rounded-md">Hamachi Maki</button>
-                </div>
-            </div>
-        </form>    
-    );
-};
+            {
+                items.map((category, categoryIndex) => {
+                    return(
+                        <div key={categoryIndex} className="border w-full pt-5 p-1 flex flex-col items-start gap-2">
+                            <h3 className="text-gray-500">{category.category}</h3>
+                            <div>
+                                {category.items.map((item, itemIndex) => {
+                                    return(
+                                        <button key={itemIndex} data-price={item.price} type="button" value={10} name="Maguro Maki" onClick={itemClick} className="border bg-white p-4 rounded-md">{item.name}</button>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>    
+    )
+}
 
-export default ItemButtons;
+export default ItemButtons
--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,59 +1,59 @@
 'use client'
 import { useState, useEffect } from "react"
-import ItemButtons from "./itemButtons";
+import ItemButtons from "./itemButtons"
 
 interface OrderProps {
-    value: number;
-    name: string;
-    order: [];
+    price: number
+    name: string
+    order: []
 }
 
 interface OrderItem {
-    id: number; // Unique identifier
-    name: string;
-    value: number;
+    id: number// Unique identifier
+    name: string
+    price: number
 }
   
 
 const Order: React.FC<OrderProps> = ({ }) => {
-    const [order, setOrder] = useState<OrderItem[]>([]);
-    const [selectedItems, setSelectedItems] = useState<OrderItem[]>([]);
+    const [order, setOrder] = useState<OrderItem[]>([])
+    const [selectedItems, setSelectedItems] = useState<OrderItem[]>([])
     
     useEffect(() => {
         setOrder([])
     }, [])
 
-    const addItem = (name: string, value: number) => {
-        const newItem: OrderItem = { id: (!order[order.length-1]) ? 0 : order[order.length-1].id +1, name: name, value: value };
-        setOrder((prevOrder) => [...prevOrder, newItem]);
-    };
+    const addItem = (name: string, price: number) => {
+        const newItem: OrderItem = { id: (!order[order.length-1]) ? 0 : order[order.length-1].id +1, name: name, price: price }
+        setOrder((prevOrder) => [...prevOrder, newItem])
+    }
 
       // Toggle item selection
     const toggleItem = (item: OrderItem) => {
         setSelectedItems((prevItems) => {
         if (prevItems.some((i) => i.id === item.id)) {
-            return prevItems.filter((i) => i.id !== item.id); // Deselect if already selected
+            return prevItems.filter((i) => i.id !== item.id)// Deselect if already selected
         } else {
-            return [...prevItems, item]; // Select if not selected
+            return [...prevItems, item]// Select if not selected
         }
-        });
-    };
+        })
+    }
     
       // Helper function to determine if an item is selected
       const isSelected = (item: OrderItem) =>
-        selectedItems.some((selectedItem) => selectedItem.id === item.id);
+        selectedItems.some((selectedItem) => selectedItem.id === item.id)
     
 
       const deleteSelectedItems = () => {
         setOrder((prevItems) =>
           prevItems.filter((item) => !selectedItems.some((i) => i.id === item.id))
-        );
-        setSelectedItems([]); // Clear selected items after deletion
-      };
+        )
+        setSelectedItems([])// Clear selected items after deletion
+      }
 
     return(
-        <div className='w-full h-full flex px-4'>
-            <div className="w-1/2 flex flex-col items-center bg-white border font-medium">
+        <form className='w-full h-full flex px-4'>
+            <div className="w-1/2 flex flex-col items-center bg-white border font-medium overflow-auto">
                 <h2 className="text-xl w-full border text-center">Order</h2>
                 <div className="w-full">
                     {order.map((item) => {
@@ -64,9 +64,9 @@ const Order: React.FC<OrderProps> = ({ }) => {
                                 className={`p-2 pl-6 w-full flex flex-row text-start justify-between border cursor-pointer ${isSelected(item) ? 'bg-gray-300' : ''}`} // Highlight selected item
                             >
                                 <ol>{item.name}</ol>
-                                <ol>${item.value}</ol>    
+                                <ol>${item.price}</ol>    
                             </div>
-                        );
+                        )
                     })}
                 </div>
                 <button disabled={selectedItems.length === 0} onClick={deleteSelectedItems} className="border bg-red-500 text-white p-2 rounded-md mt-4">
@@ -74,8 +74,8 @@ const Order: React.FC<OrderProps> = ({ }) => {
                 </button>
             </div>
             <ItemButtons addItem={addItem}></ItemButtons>
-        </div>
+        </form>
     )
 }
 
-export default Order;
+export default Order
