7393b3abebaba2691972b707aeaf9790f222c89a
unknown <saradawsone@gmail.com>
2024-12-03 15:53:04 -0700

    supabase database template added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import LoginForm from "@/app/login/loginForm";
+import { createClient } from './api/supabase/sever'
+import { cookies } from 'next/headers'
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies()
+  const supabase = createClient(cookieStore)
+
   return (
     <div className="h-full w-1/3">
         <LoginForm />
